876f9f68d75895e8d9f58b81a67a10cfe248ebbb
Gin Derick <ginderick@pop-os.localdomain>
2023-03-29 17:32:10 +0800

    update routes

--- src/api.py
+++ src/api.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from src.text import text_endpoints
+from src.translate import translate_endpoints
 
 api_router = APIRouter()
 
 api_router.include_router(text_endpoints.router)
+api_router.include_router(translate_endpoints.router)
