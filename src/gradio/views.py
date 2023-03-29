c5d06c90e922d448e057285f50d32ec709496ee4
Gin Derick <ginderick@pop-os.localdomain>
2023-03-29 17:46:06 +0800

    add gradio view

---
+++ src/gradio/views.py
@@ -0,0 +1,19 @@
+import gradio as gr
+from src.text import text_service
+from src.translate import translate_service
+
+with gr.Blocks() as demo:
+    with gr.Row():
+        with gr.Column():
+            seed = gr.Text(label="Input Phrase")
+        with gr.Column():
+            english = gr.Text(label="Generated Text")
+            translation = gr.Text(label="Translated Text")
+    btn = gr.Button("Generate")
+    btn_translate = gr.Button("Translate")
+    btn.click(text_service.generate, inputs=[seed], outputs=[english])
+    btn_translate.click(
+        translate_service.translate, inputs=[english], outputs=[translation]
+    )
+
+    gr.Examples(["My name is Clara and I am"], inputs=[seed])
