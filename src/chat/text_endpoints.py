40978be2bd88a73ccc45dc36de604a9749b57b4f
Gin Derick <ginderick@pop-os.localdomain>
2023-03-29 16:33:23 +0800

    update generate endpoint

--- src/chat/text_endpoints.py
+++ src/chat/text_endpoints.py
@@ -1,12 +1,13 @@
 from fastapi import APIRouter
 
 from src.chat import text_service
+from src.chat.text_pydantic_models import Item
 
 router = APIRouter()
 
 
-@router.get("/greet")
-async def generate():
+@router.post("/generate")
+async def generate(item: Item):
 
-    result = text_service.generate("Gin")
+    result = text_service.generate(item.text)
     return result
